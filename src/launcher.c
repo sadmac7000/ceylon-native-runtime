32e435d68667d092d64d44e1be253c7ec6febbc0
Casey Dahlin <casey.dahlin@gmail.com>
2015-06-25 20:25:58 -0400

    Make the launcher a better citizen
    Signed-off-by: Casey Dahlin <casey.dahlin@gmail.com>

--- src/launcher.c
+++ src/launcher.c
@@ -1,12 +1,33 @@
 #include <stdio.h>
+#include <stdlib.h>
 #include <stdint.h>
+#include <string.h>
 #include <err.h>
 #include <dlfcn.h>
+
+extern unsigned char launcher_help_txt[];
+extern unsigned int launcher_help_txt_len;
 
 int
 main(int argc, char **argv) {
 	void *bind;
 	int64_t * (*run)();
+	int i;
+
+	for (i = 1; i < argc; i++) {
+		if (! strcmp("--_print-summary", argv[i])) {
+			printf("Executes a Ceylon program\n");
+		} else if (! strcmp("--_print-usage", argv[i])) {
+			printf("<module>\n");
+		} else if (! strcmp("--_print-description", argv[i])) {
+			printf("%*s\n", launcher_help_txt_len,
+			       launcher_help_txt);
+		} else {
+			continue;
+		}
+
+		exit(0);
+	}
 
 	if (argc != 2)
 		errx(1, "Wrong number of arguments");
