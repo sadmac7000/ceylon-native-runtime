1f21b22a01032e75c87b1e9058969c3259ff74aa
Casey Dahlin <sadmac@google.com>
2016-05-11 22:16:43 -0700

    Add ceylon.language.Basic stub
    Signed-off-by: Casey Dahlin <sadmac@google.com>

---
+++ src/Basic.c
@@ -0,0 +1,14 @@
+#include <stdint.h>
+
+#include "util.h"
+
+int64_t basic_vtable[1] = {};
+
+API_EXPORT
+int64_t *__ceylon_language_Basic_vtable = basic_vtable;
+
+API_EXPORT
+int64_t __ceylon_language_Basic_size = 2;
+
+API_EXPORT
+int64_t __ceylon_language_Basic_vtsize = sizeof(basic_vtable) / 8;
